b96f9ea9dc21d7b2e75e7aab32cd6ed98e031bcd
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-12-01 01:28:33 -0800

    feat: delete implementation

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import { promises as fsPromises } from "fs";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+    const dirPath = "./files/";
+    const fileToRemovePath = path.join(dirPath, "fileToRemove.txt");
+
+    const isOriginFileExists = await isFileExists(fileToRemovePath);
+    if (!isOriginFileExists) {
+        throw new Error("FS operation failed");
+    }
+
+    await fsPromises.remove(fileToRemovePath);
+};
+
+const isFileExists = async (path) => {
+    try {
+        const fileStats = await fsPromises.stat(path);
+        if (fileStats.isFile()) {
+            return true;
+        }
+        return false;
+    } catch (e) {
+        return false;
+    }
 };
 
 await remove();
