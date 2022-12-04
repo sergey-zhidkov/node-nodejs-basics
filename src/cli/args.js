5b6b0cda5ed34bbcd53f2a7dc91a5f10ef373328
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-12-03 21:44:42 -0800

    feat: args implementation

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+
+    const scriptArgs = process.argv.slice(2);
+
+    let result = "";
+    for (let i = 0; i < scriptArgs.length; i++) {
+        if (i % 2 === 0) {
+            const propName = scriptArgs[i].substring(2);
+            result += propName + " is ";
+        } else {
+            result += scriptArgs[i] + ", ";
+        }
+    }
+
+    console.log(result.substring(0, result.length - 2));
 };
 
 parseArgs();
