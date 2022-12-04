2454b6872474b6a278cd71e186b1b14823894f8d
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-12-03 23:07:54 -0800

    feat: read streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,9 @@
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+    const filePath = "./files/fileToRead.txt";
+    const stream = fs.createReadStream(filePath);
+    stream.pipe(process.stdout);
 };
 
 await read();
