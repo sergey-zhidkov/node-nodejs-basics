b635b6812195331116fe1b1d9e30f41027273233
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-12-04 16:38:54 -0800

    feat: compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,12 @@
+import zlib from "zlib";
+import fs from "fs";
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = zlib.createGzip();
+    const rs = fs.createReadStream("./files/fileToCompress.txt");
+    const ws = fs.createWriteStream("./files/archive.gz");
+    rs.pipe(gzip).pipe(ws);
+    // Write your code here
 };
 
 await compress();
