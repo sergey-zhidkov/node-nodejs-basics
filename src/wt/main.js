db5896a5358b5f09c6183cf144264ffcfce39a97
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-12-04 17:15:51 -0800

    feat: wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCount = os.cpus().length;
+    console.log(cpuCount);
+    const initialN = 10;
+
+    let returnedResults = 0;
+    const results = [];
+    for (let i = 0; i < cpuCount; i++) {
+        const worker = new Worker("./worker.js", { workerData: initialN + i });
+        worker.on("message", (data) => {
+            results[i] = { status: "resolved", data };
+            console.log(data, i);
+            returnedResults++;
+
+            // I know it should be a promise way with error handling
+            // but I'm lazy today
+            if (returnedResults === cpuCount) {
+                console.log(results);
+            }
+        });
+    }
 };
 
 await performCalculations();
