86eb3040b43e70dddde29a3dcfef21ee60937a31
Sergey Zhidkov <sergey.zhidkov@outlook.com>
2022-12-04 16:32:54 -0800

    feat: transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import fs from "fs";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+    const reverseString = new Transform({
+        transform(chunk, endcoding, callback) {
+            callback(null, chunk.toString().split("").reverse().join(""));
+        },
+    });
+
+    process.stdin.pipe(reverseString).pipe(process.stdout);
 };
 
 await transform();
